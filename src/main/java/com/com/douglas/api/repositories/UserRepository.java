c3877df0a9a510edd306bda9245eb62c681d2a81
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-04-23 15:25:21 -0300

    interface UserRepository

---
+++ src/main/java/com/com/douglas/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.com.douglas.api.repositories;
+
+import com.com.douglas.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
