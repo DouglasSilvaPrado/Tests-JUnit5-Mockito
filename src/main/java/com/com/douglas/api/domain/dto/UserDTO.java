be5dbffe291566a3ca0cdefa7bb54fb38cb4902c
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-04-23 16:04:15 -0300

    class UserDTO

---
+++ src/main/java/com/com/douglas/api/domain/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.com.douglas.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
