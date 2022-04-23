a118e07476e4308dbb66b7e19420276db120205f
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-04-23 16:27:57 -0300

    endpoint findAll user

--- src/main/java/com/com/douglas/api/services/UserService.java
+++ src/main/java/com/com/douglas/api/services/UserService.java
@@ -2,7 +2,11 @@ package com.com.douglas.api.services;
 
 import com.com.douglas.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
