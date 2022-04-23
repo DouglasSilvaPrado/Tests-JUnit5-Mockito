13876957310f8c861f1d8f73bae2c461a2f9fa8f
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-04-23 15:13:52 -0300

    class UserResource findById

---
+++ src/main/java/com/com/douglas/api/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.com.douglas.api.resources;
+
+import com.com.douglas.api.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+
+        return ResponseEntity.ok().body(new User(1, "Douglas", "douglas@gmail.com", "123"));
+    }
+}
